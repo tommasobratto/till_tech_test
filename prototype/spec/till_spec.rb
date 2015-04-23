0e238c08738d9d2b258578c1a5d67616c65f90cc
tommasobratto <tbratto22@gmail.com>
2015-04-23 11:04:19 +0100

    changed till test code to accomodate the new product object

--- prototype/spec/till_spec.rb
+++ prototype/spec/till_spec.rb
@@ -3,23 +3,24 @@ require_relative '../till.rb'
 describe Till do
 
   let(:till) { Till.new }
+  let(:latte) { double :product, name: 'Cafe latte', price: 4.75 }
 
   it 'should be able to take an order' do
-    till.take_order :latte
+    till.take_order latte
     expect(till.orders.count).to eq 1
   end
 
   it 'should be able to count the quantity of the same item ordered' do
-    2.times { till.take_order :latte }
+    2.times { till.take_order latte }
     till.calculate_quantity
-    expect(till.quantity[:latte]).to eq 2
+    expect(till.quantity[latte.name]).to eq 2
   end
 
   it 'should be able to calculate the line price of an item' do
-    2.times { till.take_order :latte }
+    2.times { till.take_order latte }
     till.calculate_quantity
-    till.calculate_line_price :latte
-    expect(till.line_price[:latte]).to eq 10
+    till.calculate_line_price latte
+    expect(till.line_price[latte.name]).to eq 9.50
   end
 
 end
--- prototype/till.rb
+++ prototype/till.rb
@@ -1,19 +1,16 @@
-require_relative './menu.rb'
-
 class Till
 
   attr_accessor :orders, :quantity, :line_price
   attr_reader :menu
 
   def initialize
-    load_files
     @orders = []
     @quantity = Hash.new(0)
     @line_price = {}
   end
 
   def take_order product
-    orders << product
+    orders << product.name
   end
 
   def calculate_quantity
@@ -23,7 +20,7 @@ class Till
   end
 
   def calculate_line_price product
-    line_price[product] = quantity[product] * menu[product]
+    line_price[product.name] = quantity[product.name] * product.price
   end
 
   def calculate_total_price
